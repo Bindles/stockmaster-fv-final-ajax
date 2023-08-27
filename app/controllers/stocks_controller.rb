a789f3002839fdca83d5ca310de3a6b4db110f97
= <=>
2023-08-27 18:49:05 -0400

    works-yes

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,7 @@
 class StocksController < ApplicationController   
     def search
-        Stock.new_from_lookup(params[:stock])
+        @stock = Stock.new_from_lookup(params[:stock])
+        puts @stock.inspect
         render json: @stock
     end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,8 @@ class Stock < ApplicationRecord
   IEX_API_KEY = 'sk_abbe07f159d242c5b78454e3e74386ed'
 
   def self.new_from_lookup(ticker_symbol)
-    looked_up_stock = StockQuote::Stock.new(api_key: IEX_API_KEY).quote(ticker_symbol)
+    looked_up_stock = StockQuote::Stock.quote(ticker_symbol, api_key: IEX_API_KEY)
     new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
   end
 end
+
